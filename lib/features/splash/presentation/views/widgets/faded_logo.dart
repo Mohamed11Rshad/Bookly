c938dcd2d4fa16a2680559482b2e2c220cecc1d4
Mohamed Rshad <126785911+Mohamed11Rshad@users.noreply.github.com>
2024-03-30 23:54:31 +0200

    adding fading animation to splash view

---
+++ lib/features/splash/presentation/views/widgets/faded_logo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FadedLogo extends StatelessWidget {
+  const FadedLogo({
+    super.key,
+    required this.opacityAnimation,
+  });
+
+  final Animation<double> opacityAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: opacityAnimation,
+        builder: (context, _) {
+          return FadeTransition(
+            opacity: opacityAnimation,
+            child: const Text(
+              'Read Free Books',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,43 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/faded_logo.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    opacityAnimation = Tween<double>(
+      begin: 0.0, // Initially invisible
+      end: 1.0, // Fully visible
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +45,18 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
+        AnimatedBuilder(
+            animation: opacityAnimation,
+            builder: (context, _) {
+              return FadeTransition(
+                opacity: opacityAnimation,
+                child: Image.asset(AssetsData.logo),
+              );
+            }),
         const SizedBox(
           height: 3,
         ),
-        const Text(
-          'Read Free Books',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
-        ),
+        FadedLogo(opacityAnimation: opacityAnimation),
       ],
     );
   }
