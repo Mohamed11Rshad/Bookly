f2dc44ebc844740accb4c7e98d2eb64ba5cac8a1
Mohamed Rshad <126785911+Mohamed11Rshad@users.noreply.github.com>
2024-04-22 13:49:44 +0200

    finish best seller books cubit

--- lib/features/home/presentation/manager/best_seller_cubit_cubit/best_seller_books_cubit.dart
+++ lib/features/home/presentation/manager/best_seller_cubit_cubit/best_seller_books_cubit.dart
@@ -1,9 +1,31 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_seller_books_state.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.fetchBestSellerBooks();
+    result.fold(
+      (failure) {
+        emit(
+          BestSellerBooksFailure(
+            errMessage: failure.errMessage,
+          ),
+        );
+      },
+      (books) => {
+        emit(
+          BestSellerBooksSuccess(
+            books: books,
+          ),
+        )
+      },
+    );
+  }
 }
